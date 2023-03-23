3b72ddcba826cc148b036e47919b7b27e4bb0577
Adam Hocini <adamhocini@MacBook-Pro-de-Adam.local>
2023-03-23 10:13:06 +0100

    correction error

---
+++ src/A_propos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function A_propos(props) {
+  return (
+    <div>A_propos</div>
+  )
+}
+
+A_propo.propTypes = {}
+
+export default A_propos
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <div></div>,
-    },
-]);
